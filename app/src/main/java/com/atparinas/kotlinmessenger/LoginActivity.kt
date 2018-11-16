56ce866c895dddf33800a00ca4dbdbb1549a5adb
Andy Parinas <andy.parinas@gmail.com>
2018-11-16 22:50:07 +1000

    Added login activity and format controls

---
+++ app/src/main/java/com/atparinas/kotlinmessenger/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.atparinas.kotlinmessenger
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        registrationBackTextView.setOnClickListener {
+            finish()
+        }
+
+    }
+
+}
